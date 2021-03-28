4c61dd74dadf667d6c096c3759e1929064c89e8e
VAC Efron <46462862+VACEfron@users.noreply.github.com>
2021-03-28 22:45:02 +0200

    Added loading movies from JSON

---
+++ Cinecon/JsonHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Cinecon
+{
+    public class JsonHelper
+    {
+        public static List<Movie> Movies = new List<Movie>();
+
+        public static void LoadJson()
+        {
+            Movies = JsonConvert.DeserializeObject<List<Movie>>(File.ReadAllText("Assets/movies.json"));
+        }
+    }
+
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string[] Genres { get; set; }
+        public int Room { get; set; }
+        public string[] Hours { get; set; }
+        public bool BringId { get; set; }
+    }
+}
---
+++ Cinecon/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace Cinecon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JsonHelper.LoadJson();
+        }
+    }
+}
