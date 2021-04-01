a96ef9ef4080bb7e51956c09031e58e832e78cc4
VAC Efron <46462862+VACEfron@users.noreply.github.com>
2021-04-01 22:50:09 +0200

    Create ConsoleHelper.cs

---
+++ Cinecon/ConsoleHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Cinecon
+{
+    public class ConsoleHelper
+    {
+        public static void ColorWriteLine(string text, ConsoleColor color)
+        {
+            var currentColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ForegroundColor = currentColor;
+        }
+
+        public static void WriteLogo(ConsoleColor color = ConsoleColor.White)
+        {
+            ColorWriteLine(@"   ____ _                            
+  / ___(_)_ __   ___  ___ ___  _ __  
+ | |   | | '_ \ / _ \/ __/ _ \| '_ \ 
+ | |___| | | | |  __/ (_| (_) | | | |
+  \____|_|_| |_|\___|\___\___/|_| |_|
+                                     ", color);
+        }
+    }
+}
