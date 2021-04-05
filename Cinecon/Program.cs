645f778a800a28dcd41b8c5206112062bf96f00f
VAC Efron <46462862+VACEfron@users.noreply.github.com>
2021-04-05 23:52:36 +0200

    Removed comments

--- Cinecon/Program.cs
+++ Cinecon/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Cinecon
 {
@@ -11,25 +10,20 @@ namespace Cinecon
             // Load JSON files into project.
             JsonHelper.LoadJson();
 
-            ConsoleHelper.WriteLogo(LogoType.Cinecon, ConsoleColor.Yellow);
+            StartChoice();
+        }
 
-            Console.WriteLine("Welkom bij Cinecon!\nBent u een medewerker of bezoeker?");
-
-            // We maken hier een nieuwe ChoiceMenu object aan en stoppen het in een variable.
-            // Hierbij moeten we een Dictionary<string, Action> als argument geven. Dit zijn de keuzes die de gebruiker kan selecteren
-            // en wat er gebeurt als ze deze keuzes selecteren.
+        public static void StartChoice()
+        {
             var choiceMenu = new ChoiceMenu(new Dictionary<string, Action>
             {
                 { "Bezoeker", VisitorsMenu.ShowVisitorMenu },
-                { "Medewerker", () => ConsoleHelper.ColorWriteLine("Yo ik ben een medewerker", ConsoleColor.Yellow) },
+                { "Medewerker", () => { ConsoleHelper.LogoType = LogoType.Employee; ConsoleHelper.WriteLogo(ConsoleColor.Red); } },
+                { "Exit", () => Environment.Exit(0) },
             });
 
-            // Hiermee kunnen we een keuzescherm genereren waarin de gebruiker moet kiezen tussen de keuzes die we in de dictionary hierboven hebt aangegeven.
             var choice = choiceMenu.MakeChoice();
 
-            // Value is de functie van de geselecteerde keuze die we in de dictionary hebben gestopt.
-            // We kunnen de functie callen door Value() te doen.
-            // Voorbeeld: Als de gebruiker kiest voor "bezoeker", dan wordt de functie "VisitorsMenu.ShowVisitorMenu" gecalled.
             choice.Value();
         }
     }    
