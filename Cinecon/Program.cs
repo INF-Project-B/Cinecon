c15c4866365bc8d15d56135396a89c8d9bbf2963
NuurSaid <0992392@hr.nl>
2021-04-05 17:42:47 +0200

    Bezoekers menu gemaakt
    voor snacks en films.

--- Cinecon/Program.cs
+++ Cinecon/Program.cs
@@ -20,8 +20,8 @@ namespace Cinecon
             // en wat er gebeurt als ze deze keuzes selecteren.
             var choiceMenu = new ChoiceMenu(new Dictionary<string, Action>
             {
-                { "Bezoeker", Example },
-                { "Medewerker", () => ConsoleHelper.ColorWriteLine("Yo ik ben een medewerker", ConsoleColor.Green) },
+                { "Bezoeker", VisitorsMenu.ShowVisitorMenu },
+                { "Medewerker", () => ConsoleHelper.ColorWriteLine("Yo ik ben een medewerker", ConsoleColor.Yellow) },
             });
 
             // Hiermee kunnen we een keuzescherm genereren waarin de gebruiker moet kiezen tussen de keuzes die we in de dictionary hierboven hebt aangegeven.
@@ -29,13 +29,8 @@ namespace Cinecon
 
             // Value is de functie van de geselecteerde keuze die we in de dictionary hebben gestopt.
             // We kunnen de functie callen door Value() te doen.
-            // Voorbeeld: Als de gebruiker kiest voor "bezoeker", dan wordt de functie "Example" gecalled.
+            // Voorbeeld: Als de gebruiker kiest voor "bezoeker", dan wordt de functie "VisitorsMenu.ShowVisitorMenu" gecalled.
             choice.Value();
-        }
-
-        public static void Example()
-        {
-            ConsoleHelper.ColorWriteLine("Yo ik ben een bezoeker", ConsoleColor.Cyan);
         }
     }    
 }
---
+++ Cinecon/VisitorsMenu.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cinecon
+{
+    public class VisitorsMenu
+    {
+        public static void ShowVisitorMenu()
+        {
+            ConsoleHelper.ColorWriteLine("Bezoekers Menu", ConsoleColor.Yellow);
+            var visitorsMenu = new ChoiceMenu(new Dictionary<string, Action>
+            {
+                { "Films", Films },
+                { "Snacks", Snacks },
+            });
+            var visitors = visitorsMenu.MakeChoice();
+            visitors.Value();
+        }
+
+        public static void Films()
+        {
+
+        }
+
+        public static void Snacks()
+        {
+
+        }
+    }
+}
