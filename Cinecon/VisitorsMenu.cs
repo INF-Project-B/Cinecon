eb11346790b44dcbd5d72fdb9e21db7e9fb86085
VAC Efron <46462862+VACEfron@users.noreply.github.com>
2021-04-05 17:49:23 +0200

    Renamed funcitons

--- Cinecon/VisitorsMenu.cs
+++ Cinecon/VisitorsMenu.cs
@@ -9,21 +9,24 @@ namespace Cinecon
         public static void ShowVisitorMenu()
         {
             ConsoleHelper.ColorWriteLine("Bezoekers Menu", ConsoleColor.Yellow);
+
             var visitorsMenu = new ChoiceMenu(new Dictionary<string, Action>
             {
-                { "Films", Films },
-                { "Snacks", Snacks },
+                { "Films", ShowFilms },
+                { "Snacks", ShowSnacks },
             });
+
             var visitors = visitorsMenu.MakeChoice();
+
             visitors.Value();
         }
 
-        public static void Films()
+        public static void ShowFilms()
         {
 
         }
 
-        public static void Snacks()
+        public static void ShowSnacks()
         {
 
         }
