6bdfca789bedf98ba068239946fff569534ca197
VAC Efron <46462862+VACEfron@users.noreply.github.com>
2021-04-05 20:46:26 +0200

    Added more logos

--- Cinecon/ConsoleHelper.cs
+++ Cinecon/ConsoleHelper.cs
@@ -12,14 +12,53 @@ namespace Cinecon
             Console.ForegroundColor = currentColor;
         }
 
-        public static void WriteLogo(ConsoleColor color = ConsoleColor.White)
+        public static void WriteLogo(LogoType logoType, ConsoleColor color = ConsoleColor.White)
         {
-            ColorWriteLine(@"   ____ _                            
+            string text = logoType switch
+            {
+                LogoType.Cinecon => @"   ____ _                            
   / ___(_)_ __   ___  ___ ___  _ __  
  | |   | | '_ \ / _ \/ __/ _ \| '_ \ 
  | |___| | | | |  __/ (_| (_) | | | |
   \____|_|_| |_|\___|\___\___/|_| |_|
-                                     ", color);
-        }
+                                     ",
+                LogoType.Visitor => @"  ____                     _             
+ | __ )  ___ _______   ___| | _____ _ __ 
+ |  _ \ / _ \_  / _ \ / _ \ |/ / _ \ '__|
+ | |_) |  __// / (_) |  __/   <  __/ |   
+ |____/ \___/___\___/ \___|_|\_\___|_|   
+                                         ",
+                LogoType.Employee => @"  __  __          _                        _             
+ |  \/  | ___  __| | _____      _____ _ __| | _____ _ __ 
+ | |\/| |/ _ \/ _` |/ _ \ \ /\ / / _ \ '__| |/ / _ \ '__|
+ | |  | |  __/ (_| |  __/\ V  V /  __/ |  |   <  __/ |   
+ |_|  |_|\___|\__,_|\___| \_/\_/ \___|_|  |_|\_\___|_|   
+                                                         ",
+                LogoType.Menu => @"  __  __                  
+ |  \/  | ___ _ __  _   _ 
+ | |\/| |/ _ \ '_ \| | | |
+ | |  | |  __/ | | | |_| |
+ |_|  |_|\___|_| |_|\__,_|
+                          ",
+                LogoType.Films => @"  _____ _ _               
+ |  ___(_) |_ __ ___  ___ 
+ | |_  | | | '_ ` _ \/ __|
+ |  _| | | | | | | | \__ \
+ |_|   |_|_|_| |_| |_|___/
+                          ",
+                _ => ""
+            };
+
+            ColorWriteLine(text, color);
+        }        
+    }
+
+    public enum LogoType
+    {
+        Cinecon,
+        Visitor,
+        Employee,
+        Menu,
+        Films
     }
 }
