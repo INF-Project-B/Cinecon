1575f533616dc048a182edb6f15252e783b32522
VAC Efron <46462862+VACEfron@users.noreply.github.com>
2021-04-05 23:52:42 +0200

    Update VisitorsMenu.cs

--- Cinecon/VisitorsMenu.cs
+++ Cinecon/VisitorsMenu.cs
@@ -8,74 +8,88 @@ namespace Cinecon
     {
         public static void ShowVisitorMenu()
         {
-            ConsoleHelper.WriteLogo(LogoType.Visitor, ConsoleColor.Yellow);
+            ConsoleHelper.LogoType = LogoType.Visitor;
 
             var visitorsMenu = new ChoiceMenu(new Dictionary<string, Action>
             {
                 { "Films", ShowFilms },
-                { "Menu", ShowMenu },
-            });
+                { "Menu", ShowMenu }
+            }, true);
 
             var visitorsMenuChoice = visitorsMenu.MakeChoice();
 
-            visitorsMenuChoice.Value();
+            if (visitorsMenuChoice.Key == "Terug")
+                Program.StartChoice();
+            else
+                visitorsMenuChoice.Value();
         }
 
         private static void ShowFilms()
         {
-            ConsoleHelper.WriteLogo(LogoType.Films, ConsoleColor.Yellow);
+            ConsoleHelper.LogoType = LogoType.Films;
         }
 
         private static void ShowMenu()
         {
-            ConsoleHelper.WriteLogo(LogoType.Menu, ConsoleColor.Yellow);
+            ConsoleHelper.LogoType = LogoType.Menu;
 
             var categoryChoices = new Dictionary<string, Action>();
 
             foreach (var category in JsonHelper.Menu)
                 categoryChoices[category.Name] = null;
 
-            var categoryChoiceMenu = new ChoiceMenu(categoryChoices);
+            var categoryChoiceMenu = new ChoiceMenu(categoryChoices, true);
 
             var categoryChoice = categoryChoiceMenu.MakeChoice();
 
-            ShowCategoryItems(categoryChoice.Key);            
+            if (categoryChoice.Key == "Terug")
+                ShowVisitorMenu();
+            else
+                ShowCategoryItems(JsonHelper.Menu.FirstOrDefault(x => x.Name == categoryChoice.Key));            
         }
 
-        private static void ShowCategoryItems(string categoryName)
+        private static void ShowCategoryItems(MenuCategory category)
         {
-            ConsoleHelper.WriteLogo(LogoType.Menu, ConsoleColor.Yellow);
+            ConsoleHelper.LogoType = LogoType.Menu;
 
-            ConsoleHelper.ColorWriteLine(categoryName, ConsoleColor.Yellow);
+            ConsoleHelper.ColorWriteLine($"Categorie: {category.Name}", ConsoleColor.Red);
 
             var itemChoices = new Dictionary<string, Action>();
-
-            var category = JsonHelper.Menu.FirstOrDefault(x => x.Name == categoryName);
 
             foreach (var item in category.MenuItems)
                 itemChoices[item.Name] = null;
 
-            var itemChoiceMenu = new ChoiceMenu(itemChoices);
+            var itemChoiceMenu = new ChoiceMenu(itemChoices, true);
 
             var itemChoice = itemChoiceMenu.MakeChoice();
 
-            ShowItemTypes(category, itemChoice.Key);
+            if (itemChoice.Key == "Terug")
+                ShowMenu();
+            else
+                ShowItemTypes(category, itemChoice.Key);
         }
 
         private static void ShowItemTypes(MenuCategory category, string item)
         {
-            ConsoleHelper.WriteLogo(LogoType.Menu, ConsoleColor.Yellow);
+            ConsoleHelper.LogoType = LogoType.Menu;
 
-            ConsoleHelper.ColorWriteLine($"{category.Name}\n\n{item}", ConsoleColor.Yellow);
+            ConsoleHelper.ColorWriteLine($"Categorie: {category.Name}\nProduct: {item}", ConsoleColor.Red);
 
             var typeChoices = new Dictionary<string, Action>();
 
             foreach (var type in category.MenuItems.FirstOrDefault(x => x.Name == item).ItemTypes)
                 typeChoices[$"{type.Key} - {type.Value:0.00} euro"] = null;
 
-            var typeChoiceMenu = new ChoiceMenu(typeChoices);
+            var typeChoiceMenu = new ChoiceMenu(typeChoices, true);
 
             var typeChoice = typeChoiceMenu.MakeChoice();
+
+            if (typeChoice.Key == "Terug")
+                ShowCategoryItems(category);
+            else 
+            { 
+                // Coming soon 
+            }                
         }
     }
 }
