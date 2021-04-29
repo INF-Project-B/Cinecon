274f3064e1dfbc935a8ebfa2498649250590527a
VAC Efron <46462862+VACEfron@users.noreply.github.com>
2021-04-29 17:47:50 +0200

    Organized functions

--- Cinecon/RoomManagement.cs
+++ Cinecon/RoomManagement.cs
@@ -71,6 +71,81 @@ namespace Cinecon
                 TotalRows = totalRows,
                 SeatsPerRow = seatsPerRow
             };
-        }        
+        }
+
+        public static void ShowRoomOptions()
+        {
+            ConsoleHelper.Breadcrumb = "Zalen";
+
+            var roomManagementOptions = new ChoiceMenu(new Dictionary<string, Action>
+            {
+                { "Zaal toevoegen", ShowAddRoom },
+                { "Zaal management", ShowRooms }
+            }, addBackChoice: true);
+
+            var roomManagementOptionsChoice = roomManagementOptions.MakeChoice();
+
+            if (roomManagementOptionsChoice.Key == "Terug")
+                EmployeesMenu.ShowEmployeesMenu();
+            else
+                roomManagementOptionsChoice.Value();
+        }
+
+        private static void ShowRooms()
+        {
+            ConsoleHelper.Breadcrumb = "Zalen / Zaal management";
+
+            var rooms = new Dictionary<string, Action>();
+            for (int i = 0; i < JsonHelper.Rooms.Count; i++)
+                rooms[$"Zaal {i + 1}"] = null;
+
+            var roomOptions = new ChoiceMenu(rooms, addBackChoice: true);
+
+            var roomOptionsChoice = roomOptions.MakeChoice();
+
+            if (roomOptionsChoice.Key == "Terug")
+                ShowRoomOptions();
+            else
+                ShowRoomManagement(int.Parse(roomOptionsChoice.Key.Replace("Zaal ", "")));
+        }
+
+        private static void ShowRoomManagement(int roomNumber)
+        {
+            ConsoleHelper.Breadcrumb = $"Zalen / Zaal management / Zaal {roomNumber}";
+
+            var room = GetRoom(roomNumber);
+
+            var roomInfoOptions = new ChoiceMenu(new Dictionary<string, Action>
+            {
+                { "Aantal rijen wijzigen", null },
+                { "Aantal stoelen per rij wijzigen", null },
+                { "Zaal verwijderen", null }
+            }, addBackChoice: true, $"   Zaal: {room.Number}\n   Aantal rijen: {room.TotalRows}\n   Stoelen per rij: {room.SeatsPerRow}\n   Aantal stoelen: {room.TotalSeats}\n");
+
+            var roomInfoOptionsChoice = roomInfoOptions.MakeChoice();
+
+            if (roomInfoOptionsChoice.Key == "Terug")
+                ShowRooms();
+        }
+
+        private static void ShowAddRoom()
+        {
+            var room = CreateRoomSetup();
+
+            Console.Clear();
+
+            var roomOptions = ChoiceMenu.CreateConfirmationChoiceMenu($"   Weet je zeker dat je de nieuwe zaal wilt toevoegen?\n\n" +
+                $"   Aantal rijen: {room.TotalRows}\n   Stoelen per rij: {room.SeatsPerRow}\n");
+
+            var roomOptionsChoice = roomOptions.MakeChoice();
+
+            if (roomOptionsChoice.Key == "Ja")
+            {
+                JsonHelper.Rooms.Add(room);
+                JsonHelper.UpdateJsonFiles();
+            }
+
+            ShowRoomOptions();
+        }
     }
 }
