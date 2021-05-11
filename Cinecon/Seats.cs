c001487907813a52c95fce5662b6ca6d0e4f0a3d
Arian Torabi <Arjan_t@live.nl>
2021-05-11 13:32:48 +0200

    ticketsection + placeholder(koopticket)

---
+++ Cinecon/Seats.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Cinecon
+{
+    public static class Seats 
+    {
+        public static void ChooseSeats()
+        {
+            Console.WriteLine("Je moer!!");
+        }
+    }
+}
