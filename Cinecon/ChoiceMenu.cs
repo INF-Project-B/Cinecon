029ac9f2b29172a028eeb1f2da11711911ef10dc
0992392 <80328928+NuurSaid@users.noreply.github.com>
2021-05-16 22:36:38 +0200

    ChoicemenuSeats
    Made a choicemenu to select multiple objects horizontal and vertically. You can select with enter and deselect them the same way.

--- Cinecon/ChoiceMenu.cs
+++ Cinecon/ChoiceMenu.cs
@@ -6,6 +6,7 @@ namespace Cinecon
 {
     public class ChoiceMenu
     {
+        private readonly List<List<KeyValuePair<string, Action>>> _2dChoices;
         private readonly List<KeyValuePair<string, Action>> _choices;
         private readonly string _text;
         private readonly ConsoleColor _textColor;
@@ -16,6 +17,13 @@ namespace Cinecon
                 choices["Terug"] = null;
 
             _choices = choices.ToList();
+            _text = text;
+            _textColor = textColor;
+        }
+
+        public ChoiceMenu(List<Dictionary<string, Action>> twodChoices, string text = null, ConsoleColor textColor = ConsoleColor.White)
+        {
+            _2dChoices = twodChoices.Select(x => x.ToList()).ToList();
             _text = text;
             _textColor = textColor;
         }
@@ -112,6 +120,122 @@ namespace Cinecon
                         }
                         break;
                 }
+            }
+        }
+
+        public List<List<KeyValuePair<string, Action>>> MakeAllChoice(List<List<KeyValuePair<string, Action>>> preselectedGenres = null, Room room = null)
+        {
+            Console.CursorVisible = false;
+            WriteAllMenu(0, 0, allChoices: preselectedGenres, room: room);
+
+            int indexY = 0;
+
+            int indexX = 0;
+
+            var allChoices = preselectedGenres ?? new List<List<KeyValuePair<string, Action>>>();
+
+            var choices = new List<KeyValuePair<string, Action>>();
+
+            var count = 0;
+
+            foreach (var row in allChoices)
+            {
+                foreach (var collumn in row)
+                {
+                    choices[count++] = allChoices[allChoices.IndexOf(row)][row.IndexOf(collumn)];
+                }
+            }
+
+            while (true)
+            {
+                Console.CursorTop = 0;
+
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.LeftArrow:
+                        if (indexX - 1 >= 0)
+                            indexX -= 1;
+                        else
+                        {
+                            indexX = _2dChoices[indexY].Count - 1;
+                            indexY = indexY - 1 >= 0 ? indexY - 1 : 0;
+                        }
+                        WriteAllMenu(indexY, indexX, choices, room: room);
+                        break;
+                    case ConsoleKey.RightArrow:
+                        if (indexX + 1 < _2dChoices[indexY].Count)
+                            indexX += 1;
+                        else
+                        {
+                            indexX = 0;
+                            indexY = indexY + 1 < _2dChoices.Count ? indexY + 1 : _2dChoices.Count - 1;
+                        }
+                        WriteAllMenu(indexY, indexX, choices, room: room);
+                        break;
+                    case ConsoleKey.DownArrow:
+                        indexY = indexY + 1 < _2dChoices.Count ? indexY + 1 : 0;
+                        WriteAllMenu(indexY, indexX, choices, room: room);
+                        break;
+                    case ConsoleKey.UpArrow:
+                        indexY = indexY - 1 >= 0 ? indexY - 1 : _2dChoices.Count - 1;
+                        WriteAllMenu(indexY, indexX,  choices, room: room);
+                        break;
+                    case ConsoleKey.Enter:
+                        var choice = _2dChoices[indexY][indexX];
+                        if (new[] { "Terug", "Ga door" }.Any(x => x == choice.Key))
+                        {
+                            Console.Clear();
+                            return allChoices;
+                        }
+                        if (!choices.Contains(choice))
+                            choices.Add(choice);
+                        else
+                            choices.Remove(choice);
+                        WriteAllMenu(indexY, indexX, choices, room: room);
+                        break;
+                    case ConsoleKey.Backspace:
+                        if (_choices.Any(x => x.Key == "Terug" || x.Key == "Ga door"))
+                        {
+                            Console.Clear();
+                            return allChoices;
+                        }
+                        break;
+                }
+            }
+        }
+        
+        private void WriteAllMenu(int indexY, int indexX, List<KeyValuePair<string, Action>> selectedChoices = null, List<List<KeyValuePair<string, Action>>> allChoices = null, Room room = null)
+        {
+            ConsoleHelper.WriteLogo(ConsoleColor.Red);
+            ConsoleHelper.WriteBreadcrumb();
+
+            if (!string.IsNullOrEmpty(_text))
+                ConsoleHelper.ColorWriteLine(_text, _textColor);
+
+            foreach (var row in _2dChoices)
+            {
+                foreach (var choice in row)
+                {
+                    if (new[] { "Terug", "Exit" }.Any(x => x == choice.Key))
+                        Console.WriteLine();
+
+                    bool choiceIsSelected = selectedChoices != null && selectedChoices.Contains(choice);
+
+                    var seatIsTaken = room.Seats.FirstOrDefault(x => $"{ x.Row}{ (x.Number < 10 ? "0" : "")}{ x.Number}" == choice.Key).IsTaken;
+
+                    if (choice.Key == _2dChoices[indexY].ElementAt(indexX).Key)
+                    {
+                        if (choiceIsSelected)
+                            ConsoleHelper.ColorWrite($"   [{choice.Key}]", ConsoleColor.Yellow);
+                        else
+                            ConsoleHelper.ColorWrite($"   [{choice.Key}]", ConsoleColor.Red);
+                    }
+                    else
+                    {
+                        ConsoleHelper.ColorWrite($"   [{choice.Key}]", choiceIsSelected ? ConsoleColor.Green : seatIsTaken ? ConsoleColor.DarkRed : ConsoleColor.White);
+                    }
+                }
+                Console.WriteLine();
             }
         }
 
